a7a5d0d119a6707ddb08b8e9713d1e7b0b30067f
Sakhawat Hossain <hsakhawat135@gmail.com>
2026-06-20 15:45:12 +0600

    smtp credential add

--- project/settings.py
+++ project/settings.py
@@ -172,6 +172,17 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 
+# Gmail SMTP server Settings
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'  # Gmail SMTP server
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'sakhawatdev5@gmail.com'
+EMAIL_HOST_PASSWORD = 'xytlaswqfeqhspvt'  # Gmail app password
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
+
+
 STRIPE_SECRET_KEY = config("STRIPE_SECRET_KEY")
 STRIPE_PUBLISHABLE_KEY = config("STRIPE_PUBLISHABLE_KEY")
 STRIPE_WEBHOOK_SECRET = config("STRIPE_WEBHOOK_SECRET")
